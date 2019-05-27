38ef5ec75e881a58a9b39f450cca46f6fdf4a0e4
BakerAO <dpshunter@gmail.com>
2019-05-27 15:23:43 -0500

    Update

--- 6-streams/client/src/components/streams/StreamDelete.js
+++ 6-streams/client/src/components/streams/StreamDelete.js
@@ -2,29 +2,33 @@ import React from 'react';
 import Modal from '../Modal';
 import history from '../../history';
 
-const StreamDelete = () => {
-    const actions = (
-        <>
-            <button className="ui button negative">
-                Delete
-            </button>
-            <button className="ui button">
-                Cancel
-            </button>
-        </>
-    );
+class StreamDelete extends React.Component {
+    renderActions() {
+        return (
+            <>
+                <button className="ui button negative">
+                    Delete
+                </button>
+                <button className="ui button">
+                    Cancel
+                </button>
+            </>
+        );
+    }
 
-    return (
-        <div>
-            Stream Delete
-            <Modal
-                onDismiss={() => history.push('/')}
-                title="Delete Stream"
-                content="Are you sure you want to delete this stream?"
-                actions={actions}
-            />
-        </div>
-    );
-};
+    render() {
+        return (
+            <div>
+                Stream Delete
+                <Modal
+                    onDismiss={() => history.push('/')}
+                    title="Delete Stream"
+                    content="Are you sure you want to delete this stream?"
+                    actions={this.renderActions()}
+                />
+            </div>
+        );
+    }
+}
 
 export default StreamDelete;
